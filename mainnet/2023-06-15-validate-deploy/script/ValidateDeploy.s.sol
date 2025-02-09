53e43cfcbb63e7d42f2698dd92557237de1c04e1
aleksiss <alextsig@gmail.com>
2025-02-09 11:49:14 +0200

    Update ValidateDeploy.s.sol

--- mainnet/2023-06-15-validate-deploy/script/ValidateDeploy.s.sol
+++ mainnet/2023-06-15-validate-deploy/script/ValidateDeploy.s.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.13;
 import "forge-std/Script.sol";
 import "@base-contracts/script/deploy/l1/CheckBedrockDeploy.s.sol";
 
+
 contract ValidateDeploy is Script {
     function run() public {
         CheckBedrockDeploy checkBedrockDeploy = new CheckBedrockDeploy();
